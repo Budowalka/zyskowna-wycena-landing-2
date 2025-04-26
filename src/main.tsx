b83819c069dc71d4a9a33aa71301ecb3cfab3a7f
Budowalka <peter@smova.se>
2025-04-26 19:27:15 +0300

    Dodano integrację z Microsoft Clarity oraz zaktualizowano zależności w package.json i package-lock.json.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import Clarity from '@microsoft/clarity';
+
+// Inicjalizacja Microsoft Clarity
+Clarity.init('ra9lx2qcc0');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
